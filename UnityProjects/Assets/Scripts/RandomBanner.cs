2f2c5b12ee696f02ee187aec142f4e2714dd6e95
na-sasaki <na-sasaki@colopl.co.jp>
2016-11-25 10:59:54 +0900

    AdManager追加

---
+++ UnityProjects/Assets/Scripts/RandomBanner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using NendUnityPlugin.AD;
+
+public class AdManager : MonoBehaviour 
+{
+	[SerializeField]
+	NendAdBanner[] _banners;
+	int _index;
+	
+	void Awake()
+	{
+		if(_banners == null)
+			return;
+
+		_index = Random.Range(0, _banners.Length);
+		foreach(var banner in _banners)
+		{
+			banner.Hide();
+		}
+		_banners[_index].Show();
+	}
+
+	public void Show()
+	{
+		_banners[_index].Show();
+	}
+	
+	public void Hide()
+	{
+		_banners[_index].Hide();
+	}
+}
