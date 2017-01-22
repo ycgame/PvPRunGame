30db1b0309f474c78c7166969eaaac788c589baf
Naoto <naoto3754@gmail.com>
2017-01-22 17:01:22 +0900

    ランキング修正

--- UnityProjects/Assets/Scripts/System/GameManager.cs
+++ UnityProjects/Assets/Scripts/System/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
 	public bool IsNetwork { get; private set; }
     bool _isPlaying;
     float _elapsedTime;
-	int _playerStepCnt, _opponentStepCnt;
+	//int _playerStepCnt, _opponentStepCnt;
 
     TileManager _tileManager;
 
@@ -70,8 +70,8 @@ public class GameManager : MonoBehaviour
 		Camera.main.transform.position = 10f * Vector3.back;
 
 		_elapsedTime = 0f;
-		_playerStepCnt = 0;
-		_opponentStepCnt = 0;
+		//_playerStepCnt = 0;
+		//_opponentStepCnt = 0;
 		StopAllCoroutines();
 		StartCoroutine(CountDown(3));
 	}
@@ -171,27 +171,17 @@ public class GameManager : MonoBehaviour
 		{
 			OnMoveOpponent(step, stepCnt);
 		}
-		SetDiffImage(step);
 	}
 
 	void OnMovePlayer(int step, int stepCnt)
 	{
 		Camera.main.transform.DOMoveY(_tileManager.CalcCameraY(stepCnt), 0.05f);
-		_playerStepCnt = stepCnt;
+		//_playerStepCnt = stepCnt;
 	}
 
 	void OnMoveOpponent(int step, int stepCnt)
 	{
-		_opponentStepCnt = stepCnt;
-	}
-
-	void SetDiffImage(int step)
-	{
-		if(IsNetwork == false)
-			return;
-			
-		int diff = _playerStepCnt - _opponentStepCnt;
-		var _opponentRenderer = _opponent.GetComponentInChildren<SpriteRenderer>();
+		//_opponentStepCnt = stepCnt;
 	}
 
 	//通信相手からのステップメッセージ来たとき
--- UnityProjects/Assets/Scripts/UI/UI_Ranking.cs
+++ UnityProjects/Assets/Scripts/UI/UI_Ranking.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 
 public class UI_Ranking : UIBase
 {
@@ -10,16 +11,21 @@ public class UI_Ranking : UIBase
 	[SerializeField]
 	GameObject _TimeContents;
 	[SerializeField]
+	TextMeshProUGUI _SelfRate;
+	[SerializeField]
+	TextMeshProUGUI _SelfTime;
+	[SerializeField]
 	RatingRankList _rateRankInfoList;
 	[SerializeField]
 	TimeAttackRankList _timeRankInfoList;
 	List<Dictionary<string, object>> _timeRank10 = new List<Dictionary<string, object>>(10);
 	List<Dictionary<string, object>> _rateRank10 = new List<Dictionary<string, object>>(10);
+	bool _IsShowRate = true;
 
 	void OnEnable()
 	{
 		StartCoroutine (DisplayRanking());
-		ShowRateContents();
+		ShowContents();
 	}
 
 	void OnDisable()
@@ -35,12 +41,12 @@ public class UI_Ranking : UIBase
 			yield break;
 			
 		var timeInfo = rankingInfo["time_attack"] as Dictionary<string, object>;
-		//_timeRank = NetworkUtility.ObjectToInt(timeInfo["rank"]);
+		_SelfTime.text = NetworkManager.Instance.Self.time_attack.ToString("f3");
 		var timeTop10 = timeInfo["top10"] as List<object>;
 		_timeRank10 = timeTop10.Select(x => x as Dictionary<string, object>).ToList();
 		
 		var rateInfo = rankingInfo["rate"] as Dictionary<string, object>;
-		//_rateRank = NetworkUtility.ObjectToInt(rateInfo["rank"]);
+		_SelfRate.text = NetworkManager.Instance.Self.rate.ToString();
 		var rateTop10 = rateInfo["top10"] as List<object>;
 		_rateRank10 = rateTop10.Select(x => x as Dictionary<string, object>).ToList();
 
@@ -48,16 +54,16 @@ public class UI_Ranking : UIBase
 		_rateRankInfoList.SetRankingInfo(_rateRank10);
 	}
 
-	public void ShowRateContents()
+	void ShowContents()
 	{
-		_RateContents.SetActive(true);
-		_TimeContents.SetActive(false);
+		_RateContents.SetActive(_IsShowRate);
+		_TimeContents.SetActive(!_IsShowRate);
 	}
 
-	public void ShowTimeContents()
+	public void SwichShowContents()
 	{
-		_TimeContents.SetActive(true);
-		_RateContents.SetActive(false);
+		_IsShowRate = !_IsShowRate;
+		ShowContents();
 	}
 }
 	
